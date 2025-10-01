0ad8a590c66ffde6a0cdaf2884870ae8509a0e5f
Mihir Soni <mihirsoni855@gmail.com>
2025-10-01 17:46:14 +0530

    change UPILinkPlugin.m

--- src/ios/UPILinkPlugin.m
+++ src/ios/UPILinkPlugin.m
@@ -1,59 +1,75 @@
 #import <Cordova/CDVPlugin.h>
-#import <WebKit/WebKit.h>
+#import <UIKit/UIKit.h>
 
-@interface UPILinkPlugin : CDVPlugin <WKNavigationDelegate>
+@interface UPILinkPlugin : CDVPlugin
+- (void)open:(CDVInvokedUrlCommand*)command;
 @end
 
 @implementation UPILinkPlugin
 
-- (void)pluginInitialize {
-    if ([self.webView isKindOfClass:[WKWebView class]]) {
-        WKWebView *wkWebView = (WKWebView*)self.webView;
-        wkWebView.navigationDelegate = self;
-    }
-}
+// JS will call: cordova.exec(successCb, errorCb, "UPILinkPlugin", "open", [url]);
+- (void)open:(CDVInvokedUrlCommand*)command {
+    NSLog(@"[UPILinkPlugin] open() called");
 
-- (void)webView:(WKWebView*)webView
-decidePolicyForNavigationAction:(WKNavigationAction*)navigationAction
-decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
-
-    NSURL *url = navigationAction.request.URL;
-    NSString *scheme = url.scheme.lowercaseString;
-
-    if ([scheme hasPrefix:@"upi"] ||
-        [scheme hasPrefix:@"phonepe"] ||
-        [scheme hasPrefix:@"paytmmp"] ||
-        [scheme hasPrefix:@"gpay"] ||
-        [scheme hasPrefix:@"tez"] ||
-        [scheme hasPrefix:@"bhim"] ||
-        [scheme hasPrefix:@"amazonpay"] ||
-        [scheme hasPrefix:@"mobikwik"] ||
-        [scheme hasPrefix:@"freecharge"] ||
-        [scheme hasPrefix:@"truecaller"] ||
-        [scheme hasPrefix:@"whatsapp"]) {
-
-        if ([[UIApplication sharedApplication] canOpenURL:url]) {
-            if (@available(iOS 10.0, *)) {
-                [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
-            } else {
-                [[UIApplication sharedApplication] openURL:url];
-            }
-        } else {
-            UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Error"
-                message:@"No UPI supported application found"
-                preferredStyle:UIAlertControllerStyleAlert];
-            UIAlertAction* ok = [UIAlertAction actionWithTitle:@"OK"
-                style:UIAlertActionStyleDefault
-                handler:nil];
-            [alert addAction:ok];
-            [self.viewController presentViewController:alert animated:YES completion:nil];
-        }
-
-        decisionHandler(WKNavigationActionPolicyCancel);
+    if (command.arguments.count == 0) {
+        NSLog(@"[UPILinkPlugin] ERROR: No URL received");
+        CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                    messageAsString:@"No URL provided"];
+        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
         return;
     }
 
-    decisionHandler(WKNavigationActionPolicyAllow);
+    NSString *urlStr = [command.arguments objectAtIndex:0];
+    NSLog(@"[UPILinkPlugin] Received URL: %@", urlStr);
+
+    NSURL *url = [NSURL URLWithString:urlStr];
+    if (!url) {
+        NSLog(@"[UPILinkPlugin] ERROR: Invalid URL string");
+        CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                    messageAsString:@"Invalid URL"];
+        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        return;
+    }
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIApplication *application = [UIApplication sharedApplication];
+        BOOL canOpen = [application canOpenURL:url];
+        NSLog(@"[UPILinkPlugin] canOpenURL? %@", canOpen ? @"YES" : @"NO");
+
+        if (canOpen) {
+            if (@available(iOS 10.0, *)) {
+                [application openURL:url options:@{} completionHandler:^(BOOL success) {
+                    NSLog(@"[UPILinkPlugin] openURL success? %@", success ? @"YES" : @"NO");
+                    if (success) {
+                        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                                                    messageAsString:@"Opened UPI app"];
+                        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+                    } else {
+                        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                                    messageAsString:@"Failed to open UPI app"];
+                        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+                    }
+                }];
+            } else {
+                BOOL success = [application openURL:url];
+                NSLog(@"[UPILinkPlugin] openURL (legacy) success? %@", success ? @"YES" : @"NO");
+                if (success) {
+                    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                                                messageAsString:@"Opened UPI app"];
+                    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+                } else {
+                    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                                messageAsString:@"Failed to open UPI app"];
+                    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+                }
+            }
+        } else {
+            NSLog(@"[UPILinkPlugin] No app can handle scheme: %@", url.scheme);
+            CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                        messageAsString:@"No UPI app installed"];
+            [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        }
+    });
 }
 
 @end
